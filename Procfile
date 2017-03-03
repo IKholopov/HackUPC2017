f4d31082706b8e4cd3de0b14f6b35c516ba089e1
ikholopov <kholopov96@gmail.com>
2017-03-04 00:55:06 +0300

    AND

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hackupc.wsgi --chdir hackupc
--- hackupc/hackupc/wsgi.py
+++ hackupc/hackupc/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-application = Cling(get_wsgi_application())
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackupc.settings")
+
+application = Cling(get_wsgi_application())
