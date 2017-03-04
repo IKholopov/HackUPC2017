eb95d8aa1cde9630f16183d050225754955b297f
zerts <zertsalov96@yandex.ru>
2017-03-04 19:53:19 +0300

    side panels created

--- hackupc/static/app.jsx
+++ hackupc/static/app.jsx
@@ -1,4 +1,43 @@
+class Root extends React.Component {
+    state = {
+        display: 0,
+    };
+
+    changeDisplay(screen) {
+        console.log(screen);
+        this.setState({display : screen})
+    }
+
+    render() {
+        let st = this.state.display;
+
+        return (
+            <div className="container-fluid" style={{height: '100%'}}>
+                <div className="row" style={{height: '100%'}}>
+                    <div className="col-md-2" style={{height: '100%', 'display': 'table-cell', 'verticalAlign': 'middle'}}>
+                        <div className="buttons">
+                            <div className="row">
+
+                                <button type="button" className="btn btn-primary" onClick={this.changeDisplay.bind(this, 0)} style={{'marginBottom': '40px', 'marginLeft' : '40px'}}>0</button>
+                            </div>
+                            <div className="row">
+                                <button type="button" className="btn btn-primary" onClick={this.changeDisplay.bind(this, 1)} style={{'marginBottom': '40px', 'marginLeft': '40px'}}>1</button>
+                            </div>
+                            <div className="row">
+                                <button type="button" className="btn btn-primary" onClick={this.changeDisplay.bind(this, 2)} style={{'marginBottom': '40px', 'marginLeft': '40px'}}>2</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-10">
+                        <h1> hello, {st} </h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementsByClassName('root')[0]
+    <Root />,
+    document.getElementsByClassName('root')[0]
 );
