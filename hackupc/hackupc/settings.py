5625aa0f205c305f2a9ca9b36788225a8725e476
ikholopov <kholopov96@gmail.com>
2017-03-05 06:57:32 +0300

    deploy

--- hackupc/hackupc/settings.py
+++ hackupc/hackupc/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'hackupc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASE_URL = 'postgres://primary_user:OJv7ZQfYBCXLJYjTGMXL@team-panda-9001.postgresql.dbs.appsdeck.eu:30877/team_panda_9001'
+#DATABASE_URL = 'postgres://primary_user:OJv7ZQfYBCXLJYjTGMXL@team-panda-9001.postgresql.dbs.appsdeck.eu:30877/team_panda_9001'
 
 import dj_database_url
 
@@ -85,8 +85,8 @@ try:
 except KeyError:
     database_url = "file:///{}".format(os.path.join(BASE_DIR, 'db.sqlite3'))
 
-DATABASES = { 'default': #{ 'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3') }
-    dj_database_url.config(default=DATABASE_URL),
+DATABASES = { 'default': { 'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3') }
+   # dj_database_url.config(default=DATABASE_URL),
 }
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
