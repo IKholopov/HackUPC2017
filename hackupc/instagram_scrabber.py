72c905c454747f3684f024ac0d2c41485dba780c
SecurityQQ <flesh.game@mail.ru>
2017-03-05 01:28:49 +0100

    added id for instagram_scrabber

--- hackupc/instagram_scrabber.py
+++ hackupc/instagram_scrabber.py
@@ -21,14 +21,16 @@ def pictures_by_location(lat, lng, dist=5000):
     data = r.urlopen(r_url).read().decode('utf8')
     data = json.loads(data)
     results = []
+    pprint(data['data'])
     for d in data['data']:
         image_url = d['images']['standard_resolution']['url']
         link = d['link']
         lat = d['location']['latitude']
         lng = d['location']['longitude']
+        id = d['id']
         user_name = d['user']['username']
         text = d['caption']['text']
-        results.append({'image_url': image_url, 'instagram_link': link, 'lat': lat, 'lng': lng, 'user_name': user_name, 'text': text})
+        results.append({'id': id, 'image_url': image_url, 'instagram_link': link, 'lat': lat, 'lng': lng, 'user_name': user_name, 'text': text})
     return results
 
 def get_user_info(token):
@@ -42,7 +44,7 @@ def get_user_info(token):
     r_url = "https://api.instagram.com/v1/users/self/?access_token=" + str(token)
     data = r.urlopen(r_url).read().decode('utf8')
     data = json.loads(data)
-    pprint(data)
+    # pprint(data)
     return {'bio_title': data['data']['bio'],
      'user_name': data['data']['username'],
      'profile_picture': data['data']['profile_picture']}
@@ -65,16 +67,17 @@ def get_self_media(token):
         link = d['link']
         user_name = d['user']['username']
         text = d['caption']['text']
+        id = d['id']
         if d['location'] is not None:
             lat = d['location']['latitude']
             lng = d['location']['longitude']
         else:
             lat = lng = None
-        results.append({'image_url': image_url, 'instagram_link': link, 'lat': lat, 'lng': lng, 'user_name': user_name, 'text': text})
+        results.append({'id': id, 'image_url': image_url, 'instagram_link': link, 'lat': lat, 'lng': lng, 'user_name': user_name, 'text': text})
     return results
 
 
 
-# pprint(pictures_by_location(55.929597, 37.519426))
-# get_user_info()
-# get_self_media()
+# pictures_by_location(55.929597, 37.519426)
+# get_user_info(ACCESS_TOKEN)
+# get_self_media(ACCESS_TOKEN)
