d25b8c99721c4c41e5b302ec0523eac246831512
ikholopov <kholopov96@gmail.com>
2017-03-05 05:47:45 +0300

    loading page

--- hackupc/bestin/urls.py
+++ hackupc/bestin/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     url(r'^geodata$', views.geodata, name='geodata'),
     url(r'^userscore$', views.get_user_score, name='userscore'),
     url(r'^topscore$', views.get_top_scores, name='topscore'),
+    url(r'^loading', views.Loading.as_view()),
+    url(r'^loadposts', views.load_posts, name="loadposts"),
     url('', include('social_django.urls', namespace='social'))
 ]
