f5a1cffde2aa725ba6f65b254dee26d23c6ddaa5
ikholopov <kholopov96@gmail.com>
2017-03-05 07:05:42 +0300

    deploy

--- hackupc/hackupc/settings.py
+++ hackupc/hackupc/settings.py
@@ -86,7 +86,7 @@ except KeyError:
     database_url = "file:///{}".format(os.path.join(BASE_DIR, 'db.sqlite3'))
 
 DATABASES = { 'default': #{ 'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3') }
-   dj_database_url.config(default=DATABASE_URL),
+   dj_database_url.config(default=database_url),
 }
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
